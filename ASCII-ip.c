dec010f4527da2c6597f1e4554d1a5ad55adcfb2
Preetha0703 <114465528+Preetha0703@users.noreply.github.com>
2022-11-02 22:56:30 +0530

    Create ASCII-ip.c

---
+++ ASCII-ip.c
@@ -0,0 +1,13 @@
+include<stdio.h>
+int main()
+{
+    char chr;
+    printf("Enter a Character");
+    scanf("%c",&chr);
+
+    printf("You entered %c.\n",chr);
+
+    printf("ASCII value is %d",chr);
+    return 0;
+}
+
